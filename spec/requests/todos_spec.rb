20e6273afa2de10ad1956ff4b2dbad7183057393
Tim Garcia <tim.m.garcia@gmail.com>
2014-02-19 12:56:33 -0800

    Adding specs for delete and patch

--- spec/requests/todos_spec.rb
+++ spec/requests/todos_spec.rb
@@ -14,4 +14,51 @@ describe "Todos" do
       response.status.should == 200
     end
   end
+
+  describe "POST /todos" do
+    let(:data){ {todo: {title: "todo title", completed: "true"}}}
+    before do
+      @todo = Todo.create(title: "A test todo")
+    end
+
+    it "should post successful and return the todo item as json" do
+      post "/todos.json", data
+      # got a 200, the request was successful
+      response.status.should == 200
+
+      # The todo is now in the database
+      todo = Todo.find_by(title: "todo title")
+      todo.should_not be_nil
+      todo.title.should == "todo title"
+      todo.completed.should == true
+
+      JSON.parse(response.body)["id"].should == todo.id
+    end
+  end
+
+  describe "DELETE /todos/:id" do
+    before do
+      @todo = Todo.create!(title: "test todo", completed: false)
+    end
+    it "should delete the todo and return success" do
+      delete "/todos/#{@todo.id}.json"
+      response.status.should == 200
+      expect{Todo.find(@todo.id)}.to raise_error(ActiveRecord::RecordNotFound)
+    end
+  end
+
+  describe "PATCH /todos/:id" do
+    before do
+      @todo = Todo.create!(title: "test todo", completed: false)
+    end
+    let(:data) { {completed: true} }
+    it "should allow the caller to update the completed attribute on the todo" do
+      patch "/todos/#{@todo.id}.json", data
+      response.status.should == 200
+      @todo.completed.should == true
+
+      JSON.parse(response.body)["completed"].should == true
+    end
+  end
+
 end
