82d8dbf72a5291c39292f001d3da269e75e03c09
Tim Garcia <tim.m.garcia@gmail.com>
2014-02-19 08:23:08 -0800

    Fixing the column names for the json returned in the controller

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,16 +3,16 @@ class TodosController < ApplicationController
     @todos = Todo.all
     respond_to do |f|
       f.html
-      f.json { render :json => @todos, only: [:id, :text, :done]}
+      f.json { render :json => @todos, only: [:id, :title, :completed]}
     end
   end
 
   def create
-    todo_params = params.require(:todo).permit(:text, :done)
+    todo_params = params.require(:todo).permit(:title, :completed)
     @todo = Todo.create(todo_params)
 
     respond_to do |f|
-      f.json { render :json => @todo, only: [:id, :text, :done] }
+      f.json { render :json => @todo, only: [:id, :title, :completed] }
     end
   end
 
