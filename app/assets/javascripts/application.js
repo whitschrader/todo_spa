c4934d354350d06a9f201051cfae340ea963e7fa
Alex Notov <anotov@gmail.com>
2014-03-02 17:06:24 -0800

    Initialize backbone in rails app

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,4 +15,12 @@
 //= require turbolinks
 //= require jquery
 //= require jquery_ujs
+//= require underscore
+//= require backbone
+//= require spa_app
+//= require_tree ../templates
+//= require_tree ./models
+//= require_tree ./collections
+//= require_tree ./views
+//= require_tree ./routers
 //= require_tree .
---
+++ app/assets/javascripts/spa_app.js
@@ -0,0 +1,13 @@
+window.SpaApp = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  SpaApp.initialize();
+});
