65ae6cd9a0783839069a65ad95cb43a395cb7110
Whit Schrader <whit@schraderlabs.com>
2014-03-03 17:57:51 -0800

    can show, cannot add

--- app/assets/javascripts/views/todos/todos_index.js
+++ app/assets/javascripts/views/todos/todos_index.js
@@ -1,17 +1,41 @@
 SpaApp.Views.TodosIndex = Backbone.View.extend({
-  id: 'todos',
+  id: 'todos', //tells us the id of el
 
   template: HandlebarsTemplates['todos/index'],
+
+  events: {
+
+    'submit #addTodo': 'newTodo',
+    'click #removeTodo': 'removeTodo'
+  },
 
   render: function() {
     $(this.el).html(this.template());
 
     _.each(this.collection, function (someTodo) {
-      var todoHTML = HandlebarsTemplates['todos/show'](someTodo);
-      this.$el.append(todoHTML);
+      var view = new SpaApp.Views.TodosShow({ model: someTodo });
+      $(this.el).append(view.render().el);
     }, this);
 
     return this;
+  },
+
+
+  newTodo: function () {
+    var _this = this;
+    var newTodo = {
+      title: $("#todo_title").val(),
+      completed: false
+    };
+    $.ajax({
+      type: 'post',
+      url: '/todos.json',
+      context: this,
+      data: { todo: newTodo}
+    }).done(function (data) {
+      var newTodo = new SpaApp.Views.TodosShow({ model: data });
+      _this.$el.append(newTodo);
+    });   
   }
 
 });
